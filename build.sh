f492ffca43ef32b774e4da85ccd1bdfb2567bd27
ccat@unc.edu <ccat@unc.edu@c09551c7-f951-0410-a0da-45c4ecd87672>
2009-05-16 15:29:34 +0000

    Updated: install.rdf with updateKey Updated: build script to account for mccoy format of install.rdf Added: update.rdf source file (pre-mccoy munging)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # get the version number
-VERSION=`grep em:version ext/install.rdf | tr -d ' ' | sed 's/^<.*>\([^<].*\)<.*>$/\1/'`
+VERSION=`grep em:version ext/install.rdf | tr -d ' ' | sed 's/^em:version="\(.*\)"$/\1/'`
 # remove old extension build and staging area
 rm outfox*.xpi
 rm -rf build
